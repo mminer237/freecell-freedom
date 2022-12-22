d4d0df35b773a56ab67f1655146b46451db7c355
Matthew Miner <mminer237@gmail.com>
2022-12-21 22:40:51 -0600

    Generate game setups

--- src/bin/tui/main.rs
+++ src/bin/tui/main.rs
@@ -1,8 +1,10 @@
 use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+	event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+	execute,
+	terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
+use freecell_freedom::{Game, Suit};
+use rand::Rng;
 use rust_i18n::t;
 rust_i18n::i18n!("locales");
 use cursive::theme::{BorderStyle, Palette};
@@ -10,39 +12,42 @@ use cursive::traits::With;
 use cursive::views::{Button, LinearLayout, TextView};
 
 fn main() {
-    let mut siv = cursive::default();
+	let mut siv = cursive::default();
 
-    siv.set_theme(cursive::theme::Theme {
-        shadow: false,
-        borders: BorderStyle::Simple,
-        palette: Palette::default().with(|palette| {
-            use cursive::theme::BaseColor::*;
-            use cursive::theme::Color::TerminalDefault;
-            use cursive::theme::PaletteColor::*;
+	siv.set_theme(cursive::theme::Theme {
+		shadow: false,
+		borders: BorderStyle::Simple,
+		palette: Palette::default().with(|palette| {
+			use cursive::theme::BaseColor::*;
+			use cursive::theme::Color::TerminalDefault;
+			use cursive::theme::PaletteColor::*;
 
-            palette[Background] = TerminalDefault;
-            palette[View] = TerminalDefault;
+			palette[Background] = TerminalDefault;
+			palette[View] = TerminalDefault;
 
-            palette[Primary] = White.dark();
-            palette[TitlePrimary] = Red.light();
-            palette[Secondary] = Red.light();
-            palette[Highlight] = Red.dark();
-        }),
-    });
+			palette[Primary] = White.dark();
+			palette[TitlePrimary] = Red.light();
+			palette[Secondary] = Red.light();
+			palette[Highlight] = Red.dark();
+		}),
+	});
 
-    siv.add_global_callback('q', |s| s.quit());
+	siv.add_global_callback('q', |s| s.quit());
 
-    siv.add_layer(
-        LinearLayout::vertical()
-            .child(TextView::new(t!("title")))
-            .child(Button::new(t!("new_game"), |s| {
-                s.pop_layer();
-                new_game()
-            }))
-            .child(Button::new(t!("quit"), |s| s.quit())),
-    );
+	siv.add_layer(
+		LinearLayout::vertical()
+			.child(TextView::new(t!("title")))
+			.child(Button::new(t!("new_game"), |s| {
+				s.pop_layer();
+				new_random_game()
+			}))
+			.child(Button::new(t!("quit"), |s| s.quit())),
+	);
 
-    siv.run();
+	siv.run();
 }
 
-fn new_game() {}
+fn new_random_game() {
+	let mut game = Game::new(rand::thread_rng().gen());
+	
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,81 @@
+use strum::IntoEnumIterator;
+use strum_macros::EnumIter;
+use rand::prelude::*;
+use rand_chacha::ChaCha8Rng;
+
+#[derive(EnumIter)]
+pub enum Suit {
+	Spades,
+	Clubs,
+	Hearts,
+	Diamonds,
+}
+
+pub struct Card {
+	pub suit: Suit,
+	pub number: u16,
+}
+
+#[derive(Default)]
+pub struct Stack {
+	pub cards: Vec<Card>
+}
+
+#[derive(Default)]
+pub struct Foundation {
+	cards: Vec<Card>
+}
+
+#[derive(Default)]
+pub struct Game {
+	pub free_cells: [Option<Card>; 4],
+	pub foundations: [Foundation; 4],
+	pub stacks: [Stack; 8],
+}
+
+struct GameSeed(ChaCha8Rng);
+impl GameSeed {
+	fn new(seed: u64) -> Self {
+		GameSeed(ChaCha8Rng::seed_from_u64(seed))
+	}
+
+	fn get_next_number(&mut self, max: usize) -> usize {
+		self.0.gen_range(1..max)
+	}
+}
+
+impl Game {
+	pub fn new(seed_number: u64) -> Self {
+		let mut game_seed = GameSeed::new(seed_number);
+		let mut deck = Vec::new();
+		for number in 1..13u16 {
+			for suit in Suit::iter() {
+				deck.push(Card{
+					suit,
+					number
+				});
+			}
+		}
+
+		let mut stacks: [Stack; 8] = Default::default();
+		for deck_index in (0..deck.len()).rev() {
+			let stack_sizes: [usize; 8] = [7, 7, 7, 7, 6, 6, 6, 6];
+			for stack_list_index in 0..stack_sizes.len() {
+				let stack = &mut stacks[stack_list_index];
+				for stack_index in 0..stack_sizes[stack_list_index] {
+					let next_number = game_seed.get_next_number(deck_index);
+					stack.cards[stack_index] = deck.remove(next_number);
+				}
+			}
+		}
+
+		Self {
+			stacks,
+			..Default::default()
+		}
+	}
+}
+
 pub fn add(left: usize, right: usize) -> usize {
 	left + right
 }
